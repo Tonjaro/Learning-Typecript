7533a5992f907a680e4203aede61b0ef2ce072d3
Tonjaro <114664225+Tonjaro@users.noreply.github.com>
2022-09-29 12:49:04 +0100

    Added Practice Files

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+//Basic Types
+let id: number = 5
+let company: string = 'Google'
+let isPublished: boolean = true
+
+//Any types can be assigned other types after declaration
+let x: any = 'Hello'
+x = 1
+
+// Type of values in the array are determined by the name before the []
+let ids: number[] = [1,2,3,4,5]
